befc382e8d90a776b736b7a8741abf24e7305e3e
Francesco <francesco.pozzobon@studenti.unitn.it>
2018-12-11 15:47:29 +0100

    stabile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const StringRoutes = require('./function')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/:mystring', StringRoutes);
+app.use('/square', StringRoutes);
 
 app.get('/', function(req, res) {
 	res.send('Hello! The API is running');
